37639c87bead76d8c5a23071a14895de2907995a
Fernando VR <fernando.2020.pp@outlook.com>
2021-10-23 23:18:45 +0200

    Fixing unsupported locale settings French language

--- app/start-server.sh
+++ app/start-server.sh
@@ -3,5 +3,6 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
+(export LC_ALL=C) &
 (gunicorn display_stock.wsgi --user www-data --bind 0.0.0.0:8010 --timeout 600) &
 nginx -g "daemon off;"
