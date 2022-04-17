f6866676b662dc8d132d4eeb6a7b26232faae091
Fernando VR <fernando.2020.pp@outlook.com>
2022-04-17 23:10:04 +0200

    refactor: move business logic of download stock to stock_tools

--- app/display/stock_tools.py
+++ app/display/stock_tools.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import datetime
 import yfinance as yf
+
+from display.models import Stocks
 
 # Retrieves the stock quote for the given symbol
 # from Yahoo Finance as a float.
@@ -16,3 +19,62 @@ def get_fixstock_quote(symbol, diff_time, current_time):
     if not data.empty:
         price_fl = float(str("%5.3f" % data["Close"].values[-1]))
     return price_fl
+
+
+def get_all_stocks():
+    return Stocks.objects.all()
+
+
+def download_stocks():
+    # dotCode is used in Yahoo Finance (e.g ACCOR SA code is AC.PA)
+    stocks = [
+        {"name": "ACCOR", "dotCode": "AC.PA"},
+        {"name": "AIR LIQUIDE", "dotCode": "AI.PA"},
+        {"name": "AXA", "dotCode": "CS.PA"},
+        {"name": "BNP PARIBAS", "dotCode": "BNP.PA"},
+        {"name": "BOUYGUES", "dotCode": "EN.PA"},
+        {"name": "CRCAM BRIE PICARDIE", "dotCode": "CRBP2.PA"},
+        {"name": "CRCAM NORD France", "dotCode": "CNF.PA"},
+        {"name": "DANONE", "dotCode": "BN.PA"},
+        {"name": "EDENRED", "dotCode": "EDEN.PA"},
+        {"name": "ENGIE", "dotCode": "ENGI.PA"},
+        {"name": "ESSILOR LUXOTTICA", "dotCode": "EL.PA"},
+        {"name": "HERMES INTL", "dotCode": "RMS.PA"},
+        {"name": "ICADE", "dotCode": "ICAD.PA"},
+        {"name": "IDI", "dotCode": "IDIP.PA"},
+        {"name": "IPSEN", "dotCode": "IPN.PA"},
+        {"name": "LAGARDERE", "dotCode": "MMB.PA"},
+        {"name": "L'OREAL", "dotCode": "OR.PA"},
+        {"name": "METROPOLE TELEVISION", "dotCode": "MMT.PA"},
+        {"name": "MICHELIN", "dotCode": "ML.PA"},
+        {"name": "ORANGE", "dotCode": "ORA.PA"},
+        {"name": "PERNOD RICARD", "dotCode": "RI.PA"},
+        {"name": "ROTHSCHILD", "dotCode": "ROTH.PA"},
+        {"name": "RUBIS", "dotCode": "RUI.PA"},
+        {"name": "SANOFI", "dotCode": "SAN.PA"},
+        {"name": "SCHNEIDER ELECTRIC", "dotCode": "SU.PA"},
+        {"name": "SCOR", "dotCode": "SCR.PA"},
+        {"name": "THERMADOR GROUPE", "dotCode": "THEP.PA"},
+        {"name": "TOTAL ENERGIES", "dotCode": "TTE.PA"},
+        {"name": "UNIVERSAL MUSIC GROUP N.V.", "dotCode": "UMG.AS"},
+        {"name": "VEOLIA ENVIRONNEMENT", "dotCode": "VIE.PA"},
+        {"name": "VERALLIA", "dotCode": "VRLA.PA"},
+        {"name": "VIVENDI", "dotCode": "VIV.PA"},
+    ]
+    price_fl = None
+    Stocks.objects.all().delete()
+    current_time_tmp = datetime.datetime.now() + datetime.timedelta(days=1)
+    current_time = current_time_tmp.strftime("%Y-%m-%d")
+    diff_time_tmp = datetime.datetime.now() - datetime.timedelta(days=6)
+    diff_time = diff_time_tmp.strftime("%Y-%m-%d")
+    print(current_time)
+    print(diff_time)
+    for stock in stocks:
+        price_fl = get_fixstock_quote(stock["dotCode"], diff_time, current_time)
+        if price_fl is None or type(price_fl) == str:
+            price_fl = 0.0
+        stock_model = Stocks(name=stock["name"], symbol=stock["dotCode"], price=price_fl)
+        stock_model.save()
+        price_fl = None
+
+    return Stocks.objects.all()
--- app/display/views.py
+++ app/display/views.py
@@ -1,64 +1,12 @@
 from django.shortcuts import render
-from display.models import Stocks
 from django.http import HttpResponse
 from . import stock_tools as st
 import csv
 import locale
-import datetime
 
 
 def display(request) -> HttpResponse:
-    # dotCode is used in Yahoo Finance (e.g ACCOR SA code is AC.PA)
-    stocks = [
-        {"name": "ACCOR", "dotCode": "AC.PA"},
-        {"name": "AIR LIQUIDE", "dotCode": "AI.PA"},
-        {"name": "AXA", "dotCode": "CS.PA"},
-        {"name": "BNP PARIBAS", "dotCode": "BNP.PA"},
-        {"name": "BOUYGUES", "dotCode": "EN.PA"},
-        {"name": "CRCAM BRIE PICARDIE", "dotCode": "CRBP2.PA"},
-        {"name": "CRCAM NORD France", "dotCode": "CNF.PA"},
-        {"name": "DANONE", "dotCode": "BN.PA"},
-        {"name": "EDENRED", "dotCode": "EDEN.PA"},
-        {"name": "ENGIE", "dotCode": "ENGI.PA"},
-        {"name": "ESSILOR LUXOTTICA", "dotCode": "EL.PA"},
-        {"name": "HERMES INTL", "dotCode": "RMS.PA"},
-        {"name": "ICADE", "dotCode": "ICAD.PA"},
-        {"name": "IDI", "dotCode": "IDIP.PA"},
-        {"name": "IPSEN", "dotCode": "IPN.PA"},
-        {"name": "LAGARDERE", "dotCode": "MMB.PA"},
-        {"name": "L'OREAL", "dotCode": "OR.PA"},
-        {"name": "METROPOLE TELEVISION", "dotCode": "MMT.PA"},
-        {"name": "MICHELIN", "dotCode": "ML.PA"},
-        {"name": "ORANGE", "dotCode": "ORA.PA"},
-        {"name": "PERNOD RICARD", "dotCode": "RI.PA"},
-        {"name": "ROTHSCHILD", "dotCode": "ROTH.PA"},
-        {"name": "RUBIS", "dotCode": "RUI.PA"},
-        {"name": "SANOFI", "dotCode": "SAN.PA"},
-        {"name": "SCHNEIDER ELECTRIC", "dotCode": "SU.PA"},
-        {"name": "SCOR", "dotCode": "SCR.PA"},
-        {"name": "THERMADOR GROUPE", "dotCode": "THEP.PA"},
-        {"name": "TOTAL ENERGIES", "dotCode": "TTE.PA"},
-        {"name": "UNIVERSAL MUSIC GROUP N.V.", "dotCode": "UMG.AS"},
-        {"name": "VEOLIA ENVIRONNEMENT", "dotCode": "VIE.PA"},
-        {"name": "VERALLIA", "dotCode": "VRLA.PA"},
-        {"name": "VIVENDI", "dotCode": "VIV.PA"},
-    ]
-    price_fl = None
-    Stocks.objects.all().delete()
-    current_time_tmp = datetime.datetime.now() + datetime.timedelta(days=1)
-    current_time = current_time_tmp.strftime("%Y-%m-%d")
-    diff_time_tmp = datetime.datetime.now() - datetime.timedelta(days=6)
-    diff_time = diff_time_tmp.strftime("%Y-%m-%d")
-    print(current_time)
-    print(diff_time)
-    for stock in stocks:
-        price_fl = st.get_fixstock_quote(stock["dotCode"], diff_time, current_time)
-        if price_fl is None or type(price_fl) == str:
-            price_fl = 0.0
-        stock_model = Stocks(name=stock["name"], symbol=stock["dotCode"], price=price_fl)
-        stock_model.save()
-        price_fl = None
-    return render(request, "template.tmpl", {"stocks": Stocks.objects.all()})
+    return render(request, "template.tmpl", {"stocks": st.download_stocks()})
 
 
 def get_csv(request) -> HttpResponse:
@@ -67,7 +15,7 @@ def get_csv(request) -> HttpResponse:
     response["Content-Disposition"] = "attachment; filename=get_values.csv"
     writer = csv.writer(response, dialect="excel", delimiter="\t")
     writer.writerow(["sep=\t"])
-    obj = Stocks.objects.all()
-    for obj_i in obj:
-        writer.writerow([obj_i.name, locale.format("%.4f", obj_i.price)])
+    stocks = st.get_all_stocks()
+    for stock in stocks:
+        writer.writerow([stock.name, locale.format("%.4f", stock.price)])
     return response
