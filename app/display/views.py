3c7a4e8bbe1cf32a0c48b10bdf4d6ed08c8e3160
FernandoV <fernando.capfi@gmail.com>
2023-02-17 20:21:51 +0000

    feat: add Stellantis NV stock

--- app/display/views.py
+++ app/display/views.py
@@ -37,6 +37,7 @@ def display(request) -> HttpResponse:
         {"name": "SANOFI", "dotCode": "SAN.PA"},
         {"name": "SCHNEIDER ELECTRIC", "dotCode": "SU.PA"},
         {"name": "SCOR", "dotCode": "SCR.PA"},
+        {"name": "STELLANTIS NV", "dotCode": "STLA.PA"},
         {"name": "THERMADOR GROUPE", "dotCode": "THEP.PA"},
         {"name": "TOTAL ENERGIES", "dotCode": "TTE.PA"},
         {"name": "VEOLIA ENVIRONNEMENT", "dotCode": "VIE.PA"},
