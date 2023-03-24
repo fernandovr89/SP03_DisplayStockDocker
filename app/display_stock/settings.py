07d603cddec4d647f9a7988096e183d3898ea5e5
Fernando VR <fernando.2020.pp@outlook.com>
2023-03-24 20:47:25 +0100

    chore: change split character

--- app/display_stock/settings.py
+++ app/display_stock/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9d18f%z^hq0*dd4d7g%7+1&np#zxng@4rl8w1*ey_a@=5x@5*^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
